7b827d375ed048b1653bea760fb81813a79561b8
Michael <michael.boerger@gmail.com>
2014-04-04 14:55:57 -0400

    I am BUILDING my nefarious SCHEMAs

---
+++ spec/factories/buildings.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :building do
+    street_address "1 Cool Guy Lane"
+    city "Medford"
+    state "Massachusetts"
+    postal_code 02155
+    description "THIS IS SOME DUMB BUILDING"
+  end
+end
