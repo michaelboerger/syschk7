8a3c00c892016198939d3e3efc16c0d17c559c82
Eric Kelly <heroiceric@gmail.com>
2014-04-09 08:01:58 -0400

    Set up association between buildings and owner
    The has many is dependent :nullify, which means that when an owner is
    destroyed, all of the buildings that belong to that owner will have nil
    for owner_id and no longer belong to them.

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Owner do
+  it { should have_many(:buildings).dependent(:nullify) }
+
   it { should validate_presence_of(:first_name) }
-
   it { should validate_presence_of(:last_name) }
-
   it { should validate_presence_of(:email) }
 end
